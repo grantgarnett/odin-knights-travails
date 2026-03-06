ead31e4569496cc37b1ee10b74099c5b7c047f66
Grant Garnett <grantgarnett01@outlook.com>
2026-03-06 12:32:23 -0600

    Create KnightsTravails class & implementation

---
+++ lib/knights_travails.rb
@@ -0,0 +1,99 @@
+require_relative "node"
+
+class KnightsTravails
+  VERTICES = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+              [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7],
+              [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7],
+              [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7],
+              [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7],
+              [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7],
+              [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7],
+              [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7]]
+
+  KNIGHT_MOVE_DIRS = [[1, 2], [2, 1], [1, -2], [2, -1],
+                      [-1, 2], [-2, 1], [-1, -2], [-2, -1]]
+
+  def initialize
+    @board = generate_board
+  end
+
+  def generate_board
+    return_arr = []
+
+    VERTICES.each do |vertex|
+      return_arr << Node.new(vertex)
+    end
+
+    return_arr
+  end
+
+  def reset_board
+    @board.each do |node|
+      node.update_path(nil)
+    end
+  end
+
+  def knight_moves(start_pos, target)
+    return unless VERTICES.include?(start_pos) && VERTICES.include?(target)
+
+    start_node = find_node(start_pos)
+    start_node.update_path("start")
+    queue = [start_node]
+
+    until queue.empty?
+      current_node = queue.shift
+
+      if current_node.coords == target
+        shortest_path = find_shortest_path(current_node)
+        reset_board
+        return shortest_path
+      else
+        possible_next_moves = find_next_moves(current_node.coords)
+
+        possible_next_moves.compact.each do |node|
+          if node.prev_move.nil?
+            node.update_path(current_node)
+            queue << node
+          end
+        end
+      end
+    end
+  end
+
+  def find_next_moves(coord)
+    next_moves = []
+
+    KNIGHT_MOVE_DIRS.each do |dir|
+      next_moves << find_node([coord[0] + dir[0], coord[1] + dir[1]])
+    end
+
+    next_moves
+  end
+
+  def find_shortest_path(target)
+    path_arr = []
+    node = target
+
+    until node == "start"
+      path_arr << node.coords
+      node = node.prev_move
+    end
+
+    puts "You made it in #{path_arr.size - 1} moves! Here's your path: "
+    path_arr.reverse.each do |coord|
+      print  "#{coord} \n"
+    end
+  end
+
+  def find_node(position)
+    @board.find { |square| square.coords == position }
+  end
+end
+
+test = KnightsTravails.new
+
+test.knight_moves([0, 0], [1, 0])
+test.knight_moves([0, 0], [3, 3])
+test.knight_moves([0, 0], [7, 7])
+test.knight_moves([3, 4], [7, 6])
+test.knight_moves([1, 1], [1, 1])
